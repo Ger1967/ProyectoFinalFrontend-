9770bbc8e614e9dd1055bcc2a2ff304745ddae58
Bruno Benvenuto <brunobenvenuto23@gmail.com>
2023-02-22 19:53:33 -0300

    comienzo log in

--- proyecto-final/src/components/Login/Login.jsx
+++ proyecto-final/src/components/Login/Login.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "./login.css";
 
 function Login() {
-  return <div>Login</div>;
+  const [usuario, setUsuario] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form onSubmit={}>
+      <div>
+        <label htmlFor="usuario">Usuario</label>
+        <input
+          type="email"
+          required
+          placeholder="Complete con su e-mail"
+          name="Email"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Contraseña</label>
+        <input
+          type="password"
+          required
+          placeholder="Contraseña"
+          name="Password"
+        />
+      </div>
+      <button type="submit">Ingresar</button>
+    </form>
+  );
 }
 
 export default Login;
