ab980941a53054332160f03932e23aa1c9d4ea35
ShindeyM <shindeymadrid@gmail.com>
2023-02-24 19:22:06 -0300

    commit detalles

--- proyecto-final/src/routes/detalles/Detalles.jsx
+++ proyecto-final/src/routes/detalles/Detalles.jsx
@@ -23,10 +23,12 @@ function Detalles() {
       <Link to="/" className="detalles-btn">
         <button>Atrás</button>
       </Link>
+
       <div className="seccion-detalles">
         <div className="detalles-img">
           <img src={arrayPublicacion[0]?.foto} alt="foto" />
         </div>
+
         <section className="detalles-body">
           <h2>Detalles de la propiedad</h2>
           <p>Tipo de propiedad: {arrayPublicacion[0]?.inmueble}</p>
@@ -35,15 +37,66 @@ function Detalles() {
           </p>
           <p>Dormitorios: {arrayPublicacion[0]?.dormitorios}</p>
           <p>Baños: {arrayPublicacion[0]?.baños}</p>
-          <p>Área del terreno: {arrayPublicacion[0]?.metraje_terreno}</p>
-          <p>Área edificada: {arrayPublicacion[0]?.metraje_edificado}</p>
-          <p>
-            Dirección: {arrayPublicacion[0]?.direccion},
-            {arrayPublicacion[0]?.barrio}, {arrayPublicacion[0]?.departamento}
-          </p>
+          <p>Área del terreno: {arrayPublicacion[0]?.metraje_terreno} m2</p>
+          <p>Área edificada: {arrayPublicacion[0]?.metraje_edificado} m2</p>
+          <div className="direccion">
+            <img
+              src="../img/ubicacion.png"
+              alt="icono"
+              className="icono-direccion"
+            />
+            <p>
+              {" "}
+              {arrayPublicacion[0]?.direccion}, {arrayPublicacion[0]?.barrio},{" "}
+              {arrayPublicacion[0]?.departamento}
+            </p>
+          </div>
         </section>
+
         <div className="contacto">
-          <h2>Contacto</h2>
+          <h2>Consultá por esta propiedad:</h2>
+
+          <form
+            action="https://formsubmit.co/shindeymadrid@gmail.com"
+            method="POST"
+          >
+            <div className="nombre-email">
+              <div className="nombre-apellido">
+                <input
+                  type="text"
+                  name="nombre"
+                  placeholder="Nombre"
+                  pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$"
+                  title="Nombre solo acepta letras y espacios en blanco"
+                  required
+                />
+              </div>
+
+              <div className="email">
+                <input
+                  type="email"
+                  name="correo"
+                  placeholder="Email"
+                  pattern="^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$"
+                  title="Formato de correo invalido"
+                  required
+                />
+              </div>
+            </div>
+            <div className="solicitud">
+              <textarea
+                name="comentarios"
+                cols="30"
+                rows="5"
+                placeholder="Ingrese aqui su consulta"
+                required
+              ></textarea>
+            </div>
+            <br />
+            <button type="submit" className="enviar">
+              Enviar
+            </button>
+          </form>
         </div>
         <section className="descripcion">
           <h2>Descripcion</h2>
@@ -51,6 +104,14 @@ function Detalles() {
         </section>
         <div className="ubicacion">
           <h2>Ubicacion</h2>
+          <iframe
+            src={arrayPublicacion[0]?.mapa}
+            width="600"
+            height="450"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </div>
